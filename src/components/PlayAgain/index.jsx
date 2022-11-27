6c3b57ef8f9d963f88ee5756eab7141ed29f13ed
Steven Altemose <stevenaltemose@gmail.com>
2022-11-26 21:51:47 -0500

    fixed play again and reset bugs

--- src/components/PlayAgain/index.jsx
+++ src/components/PlayAgain/index.jsx
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import Board from "../Board";
 
-function PlayAgain({handlePlayAgainClick}) {
-
-    return (
-        <div> 
-            <div 
-                className="game-over bg-gray-200 rounded-sm hover:bg-gray-300 font-semibold h-10 cursor-pointer mb-2"
-                onClick={event => handlePlayAgainClick(true)}
-            >Play Again</div>
-        </div>
-    );
+function PlayAgain({ setToPlayAgain }) {
+  return (
+    <div>
+      <div
+        className="game-over bg-gray-200 rounded-sm hover:bg-gray-300 font-semibold h-10 cursor-pointer mb-2"
+        onClick={(event) => setToPlayAgain(true)}
+      >
+        Play Again
+      </div>
+    </div>
+  );
 }
 
 export default PlayAgain;
--- src/components/__tests__/tests.test.js
+++ src/components/__tests__/tests.test.js
@@ -1,32 +1,25 @@
-import {render, screen, cleanup} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Score from '../Score'
-import App from '../../App';
-import Button from '@mui/material/Button';
+import { render, screen, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Score from "../Score";
+import App from "../../App";
+import Button from "@mui/material/Button";
 
 afterEach(() => {
-    cleanup();
+  cleanup();
 });
 
-// Testing if the react test works
-test('test', () => {
-    expect(true).toBe(true);
-})
-
 // Testing the title of the Landing Page
-test ('renders the landing page', () => {
-    render(<App/>);
-    const linkElement = screen.getByText(/What's The Word/i);
-    expect(linkElement).toBeInTheDocument();
+test("renders the landing page", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/What's The Word/i);
+  expect(linkElement).toBeInTheDocument();
 
-    /*const scoreElement = screen.getByTestId('game-display');
+  /*const scoreElement = screen.getByTestId('game-display');
     expect(scoreElement).toHaveTextContent('0');*/
 });
 
-test ('renders the Play Button', () => {
-    render(<App />);
-    const Button = screen.getByRole('button', {name: /PLAY/i});
-    expect(Button).toHaveClass('p-2');
+test("renders the Play Button", () => {
+  render(<App />);
+  const Button = screen.getByRole("button", { name: /PLAY/i });
+  expect(Button).toHaveClass("p-2");
 });
-
-
